3efbe6bd064b6c58f2b293a8c4e524c6d091cd95
John <john.26sfj@gmail.com>
2026-03-26 13:34:47 +0100

    Now it works

--- src/components/Every24Hours.jsx
+++ src/components/Every24Hours.jsx
@@ -1,30 +1,13 @@
+import styles from "./css/Every24Hours.module.css"
+
 export default function Every24Hours ({chunk, temps}) {
+    let output = [];
+    for (let i = 0; i < chunk.length; i++) {
+        output.push(<div key={i} className={styles.goodShow}><p>{chunk[i]}</p><p>{temps[i]}℃</p></div>);
+    }
     return (
         <>
-            <p>{chunk[0]} - {temps[0].toFixed(1)}℃</p>
-            <p>{chunk[1]} - {temps[1]}℃</p>
-            <p>{chunk[2]} - {temps[2]}℃</p>
-            <p>{chunk[3]} - {temps[3]}℃</p>
-            <p>{chunk[4]} - {temps[4]}℃</p>
-            <p>{chunk[5]} - {temps[5]}℃</p>
-            <p>{chunk[6]} - {temps[6]}℃</p>
-            <p>{chunk[7]} - {temps[7]}℃</p>
-            <p>{chunk[8]} - {temps[8]}℃</p>
-            <p>{chunk[9]} - {temps[9]}℃</p>
-            <p>{chunk[10]} - {temps[10]}℃</p>
-            <p>{chunk[11]} - {temps[11]}℃</p>
-            <p>{chunk[12]} - {temps[12]}℃</p>
-            <p>{chunk[13]} - {temps[13]}℃</p>
-            <p>{chunk[14]} - {temps[14]}℃</p>
-            <p>{chunk[15]} - {temps[15]}℃</p>
-            <p>{chunk[16]} - {temps[16]}℃</p>
-            <p>{chunk[17]} - {temps[17]}℃</p>
-            <p>{chunk[18]} - {temps[18]}℃</p>
-            <p>{chunk[19]} - {temps[19]}℃</p>
-            <p>{chunk[20]} - {temps[20]}℃</p>
-            <p>{chunk[21]} - {temps[21]}℃</p>
-            <p>{chunk[22]} - {temps[22]}℃</p>
-            <p>{chunk[23]} - {temps[23]}℃</p>
+            {output}
         </>
     )
 }
